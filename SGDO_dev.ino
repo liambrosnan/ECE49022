d40500dd1039f91cb07bde39daccb0a980dd250d
braydensizelove <brayden.sizelove@gmail.com>
2021-04-13 18:09:02 -0400

    Create SGDO_dev.ino

---
+++ SGDO_dev.ino
@@ -0,0 +1,36 @@
+#include "DoorControls.h"
+#include "DistanceSensor.h"
+#include "ESP32_ARDUCAM.h"
+
+bool userDoorRequest = false;
+int fixedCode = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("SGDO Begin");
+
+  //setupWeb(); //get fixed code each startup for door controls
+  setupDistanceSensor();
+  setupDoorControl();
+  setupCam();
+  
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+  pollDistanceSensor(); //return data to send to webapp on request
+  if(userDoorRequest){
+    changeDoorState(fixedCode);
+    userDoorRequest = false;
+  }
+  pollCam(); //blocking, cannot poll cam.
+  
+  /* Potential fix:
+   * set a timer/interrupt to poll door status. On state change, send data to webapp.
+   * set interrupt for requests from webapp to either start/stop camera or change door state
+   * how do make interrupts go do working for esp in cpp? what does?
+   */
+
+}

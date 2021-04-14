53941fad8421806055258dfac8ba7f53e9e7baf8
braydensizelove <brayden.sizelove@gmail.com>
2021-04-14 11:48:50 -0400

    Update SGDO_dev.ino

--- SGDO_dev.ino
+++ SGDO_dev.ino
@@ -1,24 +1,25 @@
 #include "DoorControls.h"
 #include "DistanceSensor.h"
 #include "ESP32_ARDUCAM.h"
+#include "webApp.h"
 
 bool userDoorRequest = false;
 int fixedCode = 250;
+//bool needsInterrupt = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("SGDO Begin");
-
-  //setupWeb(); //get fixed code each startup for door controls
+  
+  setupWeb(); //get fixed code each startup for door controls
   setupDistanceSensor();
   setupDoorControl();
   setupCam();
-  
 }
 
 void loop() {
-  
+  int i = 0;
   // put your main code here, to run repeatedly:
   pollDistanceSensor(); //return data to send to webapp on request
   if(userDoorRequest){
@@ -26,7 +27,6 @@ void loop() {
     userDoorRequest = false;
   }
   pollCam(); //blocking, cannot poll cam.
-  
   /* Potential fix:
    * set a timer/interrupt to poll door status. On state change, send data to webapp.
    * set interrupt for requests from webapp to either start/stop camera or change door state
